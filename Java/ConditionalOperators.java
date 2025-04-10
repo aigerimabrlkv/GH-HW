526aae5352420c9ac5824800b55f991174e44733
aigerimabrlkv <birlikovnaaigerim@gmail.com>
2025-04-10 06:58:32 -0500

    new task

--- Java/ConditionalOperators.java
+++ Java/ConditionalOperators.java
@@ -3,8 +3,9 @@ import java.util.Scanner;
 public class ConditionalOperators {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        if ((a % 4 == 0 && a % 100 != 0) || a % 400 == 0) {
+        int studentAnswer = sc.nextInt();
+        int systemAnswer = sc.nextInt();
+        if ((studentAnswer ==1 && systemAnswer == 1) || (studentAnswer !=1 && systemAnswer != 1) ){
             System.out.println("YES");
         } else {
             System.out.println("NO");
