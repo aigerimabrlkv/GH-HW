10274e895e2553081607a10c042503e91029f059
aigerimabrlkv <birlikovnaaigerim@gmail.com>
2025-04-13 22:08:44 -0500

    new task

--- Java/Loops.java
+++ Java/Loops.java
@@ -5,11 +5,22 @@ public class Loops {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         int b = sc.nextInt();
-for(int i=a; i<=b; i++ ){
-    if(i%2 == 0){
-        System.out.println(i+" ");
-    }
+        int c = sc.nextInt();
+        int d = sc.nextInt();
+        boolean found = false;
+        int start= (int) Math.ceil(Math.sqrt(a));
+        int end= (int) Math.floor(Math.sqrt(b));
 
+
+        for (int i = start; i <= end; i++) {
+            int square = i*i;
+            System.out.println(square+" ");
+            found = true;
+        }
+
+        if (found) {
+            System.out.println(); // Print a newline if any numbers were found
+        }
+
+    }
 }
-    }
-    }
